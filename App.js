e47ac1ba092273bf4840f96ab1e80a6f48a7bcb3
Work H <aaronblutstein@Work-H.local>
2021-07-28 15:10:38 +0100

    title-change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Babbit and friends!</Text>
+      <Text>Babbit and friends magazine!</Text>
       <StatusBar style="auto" />
     </View>
   );
